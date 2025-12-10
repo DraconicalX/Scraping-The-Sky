1db823c27de99b4d33a4c3145aa8045ae5ef1829
DraconicalX <tonchen@nuevaschool.org>
2025-12-10 15:03:22 -0800

    Update Player.js

--- Player.js
+++ Player.js
@@ -53,7 +53,7 @@ class Player{
   }
   move(){
     this.stats=stats
-    this.stats = accessoryEffects(this.stats)
+    this.stats = inventory.accessoryEffects(this.stats)
     movement.setAvgs();
       if(keyIsDown(RIGHT_ARROW)){
         this.direc='R'
